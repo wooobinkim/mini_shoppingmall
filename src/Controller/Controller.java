748d70341b8541fb9d0e42e60638df1c9d894a36
석재호 <wogh9705@naver.com>
2022-03-26 20:15:01 +0900

    controller

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -1,31 +1,53 @@
 package Controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Service.ProductService;
+import Service.ProductServiceImpl;
+import Service.UserService;
+import Service.UserServiceImpl;
+
 
 @WebServlet("*.shop")
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	private UserService uSer;
+	private ProductService pSer;
+	private String url="";
 	@Override
 	public void init() throws ServletException {
-		// TODO Auto-generated method stub
-		super.init();
+		uSer = new UserServiceImpl();
+		pSer = new ProductServiceImpl();
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doPost(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		
+		try {
+			String path = request.getServletPath();
+			switch(path) {
+			case "/loginuseform.shop":
+				break;
+			}
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		if(url.startsWith("redirect:")) {
+			url = url.substring(url.indexOf(":"+1));
+			response.sendRedirect(url);
+		}else{
+			request.getRequestDispatcher(url).forward(request, response);
+		}
 	}
 
 }
