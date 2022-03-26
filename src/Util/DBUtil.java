10a21f55069e94235fda3b86dee51833bedea925
석재호 <wogh9705@naver.com>
2022-03-26 18:52:39 +0900

    틀

---
+++ src/Util/DBUtil.java
@@ -0,0 +1,38 @@
+package Util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtil {
+
+	private static final String driverName = "com.mysql.cj.jdbc.Driver";
+	private static final String url = "jdbc:mysql://127.0.0.1:3306/Posting?serverTimezone=UTC";
+	private static final String user = "ssafy";
+	private static final String pass = "ssafy";
+
+	
+	static {
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(url, user, pass);
+	}
+
+	public static void close(AutoCloseable... closeables) {
+		for (AutoCloseable c : closeables) {
+			if (c != null) {
+				try {
+					c.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
